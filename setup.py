f58596462381eb90d3c5d8bc9ddd02613b0ee687
Iubu <flyingdoji@protonmail.com>
2020-12-05 12:30:17 +0200

    setup.py dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as req:
+    dependencies = [line.strip('\n') for line in req.readlines()]
+
 setuptools.setup(
     name="ftx",
     version="0.0.1",
@@ -19,4 +22,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=dependencies,
 )
